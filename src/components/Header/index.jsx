378c3fbfa3872dd2684825da0b69ea57da222266
Augusto <augusto.oliveira.099@icloud.com>
2023-08-22 13:19:43 -0300

    Correção da rota para requisição da foto de perfil

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,7 +14,7 @@ export function Header() {
     signOut();
   }
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}files/${user.avatar}` : avatarPlaceholder;
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
   const altImg = `Foto do usuário ${user.name}`
 
   console.log(avatarUrl)
